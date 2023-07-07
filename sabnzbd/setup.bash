f6c5ba2b5481d0eec0d2a9ebbe4caa9eebae2554
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-07-07 12:26:01 -0400

    Update setup.bash

--- sabnzbd/setup.bash
+++ sabnzbd/setup.bash
@@ -4,34 +4,34 @@ scriptVersion="1.0"
 ######## Package dependencies installation
 InstallRequirements () {
   echo "Installing Required Packages..."
-  apk add -U --update --no-cache curl jq python3-dev py3-pip git ffmpeg &>/dev/null
-  pip install --upgrade --no-cache-dir -U yq &>/dev/null
+  apk add -U --update --no-cache curl jq python3-dev py3-pip git ffmpeg
+  pip install --upgrade --no-cache-dir -U yq
   echo "Done"
-  
-  if [ ! -f /config/scripts/sma/manual.py ]; then
-    echo "************ setup SMA ************"
-  	echo "************ setup directory ************"
-  	mkdir -p /config/scripts/sma
-  	echo "************ download repo ************"
-  	git clone https://github.com/mdhiggins/sickbeard_mp4_automator.git /config/scripts/sma
-  	mkdir -p /config/scripts/sma/config
-  	echo "************ create logging file ************"
-  	mkdir -p /config/scripts/sma/config
-  	touch /config/scripts/sma/config/sma.log
-  	chgrp users /config/scripts/sma/config/sma.log
-  	chmod g+w /config/scripts/sma/config/sma.log
-  	echo "************ install pip dependencies ************"
-  	pip install --upgrade pip --no-cache-dir 
-   	pip install -r /config/scripts/sma/setup/requirements.txt --no-cache-dir 
-	chmod 777 -R /config/scripts/sma
+  if [ -d /config/scripts/sma ]; then
+    rm -rf /config/scripts/sma
   fi
+  echo "************ setup SMA ************"
+  echo "************ setup directory ************"
+  mkdir -p /config/scripts/sma
+  echo "************ download repo ************"
+  git clone https://github.com/mdhiggins/sickbeard_mp4_automator.git /config/scripts/sma
+  mkdir -p /config/scripts/sma/config
+  echo "************ create logging file ************"
+  mkdir -p /config/scripts/sma/config
+  touch /config/scripts/sma/config/sma.log
+  echo "************ install pip dependencies ************"
+  pip install --upgrade pip --no-cache-dir 
+  pip install -r /config/scripts/sma/setup/requirements.txt --no-cache-dir 
+  chmod 777 -R /config/scripts/sma
 }
 
 InstallRequirements
 
 mkdir -p /config/scripts
 chmod 777 /config/scripts
+echo "Downloading SMA config: /config/scripts/sma.ini"
 curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sabnzbd/sma.ini -O /config/scripts/sma.ini
+echo "Downloading Video script config: /config/scripts/video.bash"
 curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sabnzbd/video.bash -O /config/scripts/video.bash
 chmod 777 -R /config/scripts
 exit
