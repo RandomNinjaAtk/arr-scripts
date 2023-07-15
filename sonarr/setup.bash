54bcdc866b551c69a31def3573d23e5e7dc34829
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-07-15 15:37:08 -0400

    Recyclarr filename fix

--- sonarr/setup.bash
+++ sonarr/setup.bash
@@ -58,7 +58,7 @@ curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sonarr/In
 echo "Done"
 
 echo "Download Recyclarr service..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/universal/services/Recycalarr -o /custom-services.d/Recyclarr
+curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/universal/services/Recyclarr -o /custom-services.d/Recyclarr
 echo "Done"
 
 echo "Download YoutubeSeriesDownloader service..."
