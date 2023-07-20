e013b0d1b9000ac7c8e7aeac8d8a91a27c8e9c00
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-07-20 10:58:49 +0000

    2.5 - use universal functions

--- lidarr/Audio.service.bash
+++ lidarr/Audio.service.bash
@@ -1,31 +1,13 @@
 #!/usr/bin/with-contenv bash
-scriptVersion="2.4"
+scriptVersion="2.5"
 scriptName="Audio"
 
-log () {
-  m_time=`date "+%F %T"`
-  echo $m_time" :: $scriptName :: $scriptVersion :: "$1
-}
-
-logfileSetup () {
-  # auto-clean up log file to reduce space usage
-  if [ -f "/config/logs/$scriptName.txt" ]; then
-  	find /config/logs -type f -name "$scriptName.txt" -size +1024k -delete
-  fi
-  
-  if [ ! -f "/config/logs/$scriptName.txt" ]; then
-      touch "/config/logs/$scriptName.txt"
-      chmod 666 "/config/logs/$scriptName.txt"
-  fi
-}
-
-# Create Log, start writing...
-logfileSetup
-exec &> >(tee -a "/config/logs/$scriptName.txt")
+### Import Settings
+source /config/extended.conf
+#### Import Functions
+source /config/extended/functions
 
 verifyConfig () {
-  #### Import Settings
-  source /config/extended.conf
 
   if [ "$enableAudio" != "true" ]; then
     log "Script is not enabled, enable by setting enableAudio to \"true\" by modifying the \"/config/extended.conf\" config file..."
@@ -42,42 +24,6 @@ verifyConfig () {
   fi
 
   audioPath="$downloadPath/audio"
-}
-
-getArrAppInfo () {
-  # Get Arr App information
-  if [ -z "$arrUrl" ] || [ -z "$arrApiKey" ]; then
-    arrUrlBase="$(cat /config/config.xml | xq | jq -r .Config.UrlBase)"
-    if [ "$arrUrlBase" == "null" ]; then
-      arrUrlBase=""
-    else
-      arrUrlBase="/$(echo "$arrUrlBase" | sed "s/\///g")"
-    fi
-    arrName="$(cat /config/config.xml | xq | jq -r .Config.InstanceName)"
-    arrApiKey="$(cat /config/config.xml | xq | jq -r .Config.ApiKey)"
-    arrPort="$(cat /config/config.xml | xq | jq -r .Config.Port)"
-    arrUrl="http://127.0.0.1:${arrPort}${arrUrlBase}"
-  fi
-}
-
-verifyApiAccess () {
-  until false
-  do
-    arrApiTest=""
-    arrApiVersion=""
-    if [ "$arrPort" == "8989" ] || [ "$arrPort" == "7878" ]; then
-      arrApiVersion="v3"
-    elif [ "$arrPort" == "8686" ] || [ "$arrPort" == "8787" ]; then
-      arrApiVersion="v1"
-    fi
-    arrApiTest=$(curl -s "$arrUrl/api/$arrApiVersion/system/status?apikey=$arrApiKey" | jq -r .instanceName)
-    if [ "$arrApiTest" == "$arrName" ]; then
-      break
-    else
-      log "$arrName is not ready, sleeping until valid response..."
-      sleep 1
-    fi
-  done
 }
 
 Configuration () {
