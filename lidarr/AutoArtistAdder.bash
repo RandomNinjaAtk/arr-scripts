4f3bb3afd0999fce175691a9aac2526b360b0195
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-07-20 11:00:04 +0000

    1.8 - use universal functions

--- lidarr/AutoArtistAdder.bash
+++ lidarr/AutoArtistAdder.bash
@@ -1,31 +1,13 @@
 #!/usr/bin/with-contenv bash
-scriptVersion="1.7"
+scriptVersion="1.8"
 scriptName="AutoArtistAdder"
 
-log () {
-  m_time=`date "+%F %T"`
-  echo $m_time" :: $scriptName :: $scriptVersion :: "$1
-}
-
-logfileSetup () {
-  # auto-clean up log file to reduce space usage
-  if [ -f "/config/logs/$scriptName.txt" ]; then
-  	find /config/logs -type f -name "$scriptName.txt" -size +1024k -delete
-  fi
-  
-  if [ ! -f "/config/logs/$scriptName.txt" ]; then
-      touch "/config/logs/$scriptName.txt"
-      chmod 666 "/config/logs/$scriptName.txt"
-  fi
-}
-
-# Create Log, start writing...
-logfileSetup
-exec &> >(tee -a "/config/logs/AutoArtistAdder.txt")
+### Import Settings
+source /config/extended.conf
+#### Import Functions
+source /config/extended/functions
 
 verifyConfig () {
-  #### Import Settings
-  source /config/extended.conf
  
   if echo "$addDeezerTopArtists $addDeezerTopAlbumArtists $addDeezerTopTrackArtists $addRelatedArtists" | grep -i "true" | read; then
     sleep 0.01
@@ -41,41 +23,6 @@ verifyConfig () {
 
 }
 
-getArrAppInfo () {
-  # Get Arr App information
-  if [ -z "$arrUrl" ] || [ -z "$arrApiKey" ]; then
-    arrUrlBase="$(cat /config/config.xml | xq | jq -r .Config.UrlBase)"
-    if [ "$arrUrlBase" == "null" ]; then
-      arrUrlBase=""
-    else
-      arrUrlBase="/$(echo "$arrUrlBase" | sed "s/\///g")"
-    fi
-    arrName="$(cat /config/config.xml | xq | jq -r .Config.InstanceName)"
-    arrApiKey="$(cat /config/config.xml | xq | jq -r .Config.ApiKey)"
-    arrPort="$(cat /config/config.xml | xq | jq -r .Config.Port)"
-    arrUrl="http://127.0.0.1:${arrPort}${arrUrlBase}"
-  fi
-}
-
-verifyApiAccess () {
-  until false
-  do
-    arrApiTest=""
-    arrApiVersion=""
-    if [ "$arrPort" == "8989" ] || [ "$arrPort" == "7878" ]; then
-      arrApiVersion="v3"
-    elif [ "$arrPort" == "8686" ] || [ "$arrPort" == "8787" ]; then
-      arrApiVersion="v1"
-    fi
-    arrApiTest=$(curl -s "$arrUrl/api/$arrApiVersion/system/status?apikey=$arrApiKey" | jq -r .instanceName)
-    if [ "$arrApiTest" == "$arrName" ]; then
-      break
-    else
-      log "$arrName is not ready, sleeping until valid response..."
-      sleep 1
-    fi
-  done
-}
 
 sleepTimer=0.5
 
