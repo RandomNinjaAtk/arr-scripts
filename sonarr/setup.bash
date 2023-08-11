8302deeed1a7e2a1d72498a1bada709f3fc50237
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-08-11 15:27:53 +0000

    Update setup.bash

--- sonarr/setup.bash
+++ sonarr/setup.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bash
 SMA_PATH="/usr/local/sma"
 
-echo "************ install packages ************" && \
+echo "************ install packages ************"
 apk add -U --update --no-cache \
 	flac \
 	opus-tools \
@@ -14,29 +14,28 @@ apk add -U --update --no-cache \
 	py3-pip \
 	gcc \
 	ffmpeg \
-	yt-dlp && \
-echo "************ install python packages ************" && \
+	yt-dlp
+echo "************ install python packages ************"
 pip install --upgrade --no-cache-dir -U \
 	excludarr \
-	yq && \
-echo "************ setup SMA ************" && \
-echo "************ setup directory ************" && \
-mkdir -p ${SMA_PATH} && \
-echo "************ download repo ************" && \
-git clone https://github.com/mdhiggins/sickbeard_mp4_automator.git ${SMA_PATH} && \
-mkdir -p ${SMA_PATH}/config && \
-echo "************ create logging file ************" && \
-mkdir -p ${SMA_PATH}/config && \
-touch ${SMA_PATH}/config/sma.log && \
-chgrp users ${SMA_PATH}/config/sma.log && \
-chmod g+w ${SMA_PATH}/config/sma.log && \
-echo "************ install pip dependencies ************" && \
-python3 -m pip install --user --upgrade pip && \	
-pip3 install -r ${SMA_PATH}/setup/requirements.txt && \
-echo "************ install recyclarr ************" && \
-mkdir -p /recyclarr && \
-wget "https://github.com/recyclarr/recyclarr/releases/latest/download/recyclarr-linux-musl-x64.tar.xz" -O "/recyclarr/recyclarr.tar.xz" && \
-tar -xf /recyclarr/recyclarr.tar.xz -C /recyclarr &>/dev/null && \
+	yq
+echo "************ setup SMA ************"
+echo "************ setup directory ************"
+mkdir -p /config/extended/sma
+echo "************ download repo ************"
+git clone https://github.com/mdhiggins/sickbeard_mp4_automator.git /config/scripts/sma
+mkdir -p /config/extended/sma/config
+echo "************ create logging file ************"
+mkdir -p /config/extended/sma/config
+touch /config/extended/sma/config/sma.log
+echo "************ install pip dependencies ************"
+pip install --upgrade pip --no-cache-dir 
+pip install -r /config/extended/sma/setup/requirements.txt --no-cache-dir 
+chmod 777 -R /config/extended/sma
+echo "************ install recyclarr ************"
+mkdir -p /recyclarr
+wget "https://github.com/recyclarr/recyclarr/releases/latest/download/recyclarr-linux-musl-x64.tar.xz" -O "/recyclarr/recyclarr.tar.xz"
+tar -xf /recyclarr/recyclarr.tar.xz -C /recyclarr &>/dev/null
 chmod 777 /recyclarr/recyclarr
 apk add --no-cache -X http://dl-cdn.alpinelinux.org/alpine/edge/community dotnet7-runtime
 
