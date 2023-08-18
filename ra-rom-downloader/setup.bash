6e0852aa3f2b22d4f3dc4b9247c759b29e53f9a5
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-08-18 11:37:59 +0000

    Update setup.bash

--- ra-rom-downloader/setup.bash
+++ ra-rom-downloader/setup.bash
@@ -25,7 +25,7 @@ chmod -R 777 ${RAHASHER_PATH}
 
 mkdir -p /custom-services.d
 echo "Download Downloader service..."
-curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/ra-rom-downloader/Downloader.bash -o /config/Downloader
+curl https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/ra-rom-downloader/Downloader.bash -o /custom-services.d/Downloader
 echo "Done"
 chmod 777 /custom-services.d/Downloader
 
