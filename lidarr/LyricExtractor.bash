70f696ed8233f8dd830750714566e1abbd4c9a84
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-09-09 11:57:43 -0400

    v1.5 - bug fix for copy pasta, remove unused function

--- lidarr/LyricExtractor.bash
+++ lidarr/LyricExtractor.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-scriptVersion="1.4"
+scriptVersion="1.5"
 scriptName="LyricExtractor"
 
 #### Import Settings
@@ -16,7 +16,6 @@ if [ "$lidarr_eventtype" == "Test" ]; then
 	exit 0	
 fi
 
-verifyConfig
 getArrAppInfo
 verifyApiAccess
 
