609c9085b3f5eb32e1c6a76171e68a505d75ac90
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2024-04-23 14:08:10 +0000

    Only run one time
    #244

--- ra-rom-downloader/setup.bash
+++ ra-rom-downloader/setup.bash
@@ -54,4 +54,8 @@ if [ ! -f /config/extended.conf ]; then
 	echo "Done"
 fi
 
+if [ -f /custom-services.d/scripts_init.bash ]; then
+   # user misconfiguration detected, sleeping...
+   sleep infinity
+fi
 exit
