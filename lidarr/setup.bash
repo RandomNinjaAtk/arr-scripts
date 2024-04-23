9c0c370b8325d0d4d55158a061c2074c6999a484
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2024-04-23 14:00:24 +0000

    Only run one time in the event of a misconfiguration...
    #244 - Protect the user from themself... not a bug fix...

--- lidarr/setup.bash
+++ lidarr/setup.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bash
 SMA_PATH="/usr/local/sma"
-version="1.2"
+version="1.3"
 
 echo "*** install packages ***" && \
 apk add -U --upgrade --no-cache \
@@ -152,4 +152,9 @@ fi
 
 chmod 777 -R /config/extended
 chmod 777 -R /root
+
+if [ -f /custom-services.d/scripts_init.bash ]; then
+   # user misconfiguration detected, sleeping...
+   sleep infinity
+fi
 exit
