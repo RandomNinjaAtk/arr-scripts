d3113f7f42435b5c2f93954d957889ae8a34365f
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2024-07-30 12:52:53 +0000

    Update functions.bash
    #87 - Fix?

--- universal/functions.bash
+++ universal/functions.bash
@@ -1,6 +1,7 @@
 log () {
   m_time=`date "+%F %T"`
-  echo $m_time" :: $scriptName :: $scriptVersion :: "$1 2>&1 | tee -a "/config/logs/$logFileName"
+  echo $m_time" :: $scriptName :: $scriptVersion :: "$1
+  echo $m_time" :: $scriptName :: $scriptVersion :: "$1 >> "/config/logs/$logFileName"
 }
 
 logfileSetup () {
