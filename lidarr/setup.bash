b80a413288f9f91cc967e2bf2fb1ded36b5eadbd
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2025-05-08 22:36:23 -0400

    v1.4 - Only execute when needed
    #321

--- lidarr/setup.bash
+++ lidarr/setup.bash
@@ -1,8 +1,20 @@
 #!/usr/bin/with-contenv bash
-set -euo pipefail
-
+scriptVersion="1.4"
 SMA_PATH="/usr/local/sma"
-version="1.3"
+
+if [ -f /config/setup_version.txt ]; then
+  source /config/setup_version.txt
+  if [ "$scriptVersion" == "$setupversion" ]; then
+    if apk list | grep installed | grep opus-tools | read; then
+      echo "Setup was previously completed, skipping..."
+      exit
+    fi
+  fi
+else
+  echo "setupversion=$scriptVersion" > /config/setup_version.txt
+fi
+
+set -euo pipefail
 
 echo "*** install packages ***" && \
 apk add -U --upgrade --no-cache \
