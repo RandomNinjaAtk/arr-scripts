ae6095d148c4a29241d1a18ce65398e24ad7feae
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2025-07-08 08:08:17 -0400

    Create UnmappedFolderCleaner.bash

---
+++ sonarr/UnmappedFolderCleaner.bash
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+scriptVersion="1.5"
+scriptName="UnmappedFolderCleaner"
+
+#### Import Settings
+source /config/extended.conf
+#### Import Functions
+source /config/extended/functions
+#### Create Log File
+logfileSetup
+
+verifyConfig () {
+
+  if [ "$enableUnmappedFolderCleaner" != "true" ]; then
+    log "Script is not enabled, enable by setting enableUnmappedFolderCleaner to \"true\" by modifying the \"/config/extended.conf\" config file..."
+    log "Sleeping (infinity)"
+    sleep infinity
+  fi
+
+  if [ -z "$unmappedFolderCleanerScriptInterval" ]; then
+    unmappedFolderCleanerScriptInterval="1h"
+  fi
+}
+
+UnmappedFolderCleanerProcess () {
+	log "Finding UnmappedFolders to purge..."
+	OLDIFS="$IFS"
+	IFS=$'\n'
+    unmappedFoldersCount=$(curl -s "$arrUrl/api/v3/rootFolder" -H "X-Api-Key: $arrApiKey" | jq -r ".[].unmappedFolders[].path" | wc -l)
+	log "$unmappedFoldersCount Folders Found!"
+	if [ $unmappedFoldersCount = 0 ]; then 
+	    log "No cleanup required, exiting..."
+	    return
+	fi
+    unmappedFolders=$(curl -s "$arrUrl/api/v3/rootFolder" -H "X-Api-Key: $arrApiKey" | jq -r ".[].unmappedFolders[].path")
+	for folder in $(echo "$unmappedFolders"); do
+	    log "Removing $folder"
+	    rm -rf "$folder"
+	done
+	IFS="$OLDIFS"
+ }
+
+
+# Loop Script
+for (( ; ; )); do
+	let i++
+	logfileSetup
+ 	log "Script starting..."
+    verifyConfig
+	getArrAppInfo
+	verifyApiAccess
+	UnmappedFolderCleanerProcess
+	log "Script sleeping for $unmappedFolderCleanerScriptInterval..."
+	sleep $unmappedFolderCleanerScriptInterval
+done
+
+exit
