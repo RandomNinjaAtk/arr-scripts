a61c06265eb0001a7e007c6313f2f0536e441147
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2025-07-31 22:09:52 -0400

    2.4 update scripts

--- sabnzbd/setup.bash
+++ sabnzbd/setup.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/with-contenv bash
-scriptVersion="2.3"
+scriptVersion="2.4"
 
 if [ -f /config/setup_version.txt ]; then
   source /config/setup_version.txt
@@ -69,6 +69,18 @@ if [ -f /config/sma.ini ]; then
     chmod 777 /config/scripts/sma.ini 
   else
     echo "File /config/scripts/sma.ini already exists. Not overwriting."
+  fi
+fi
+
+echo "Downloading SMA config: /config/scripts/sma_defaultlang.ini"
+curl "https://raw.githubusercontent.com/RandomNinjaAtk/arr-scripts/main/sabnzbd/sma_defaultlang.ini" -o /config/sma_defaultlang.ini
+if [ -f /config/sma_defaultlang.ini ]; then
+  if [ ! -f /config/scripts/sma_defaultlang.ini ]; then
+    echo "Importing /config/sma.ini to /config/scripts/sma_defaultlang.ini"
+    mv /config/sma.ini /config/scripts/sma_defaultlang.ini 
+    chmod 777 /config/scripts/sma_defaultlang.ini 
+  else
+    echo "File /config/scripts/sma_defaultlang.ini already exists. Not overwriting."
   fi
 fi
 
