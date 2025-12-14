4a243f0d3d0b8037b136416410efe7a94e239196
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2025-12-14 16:03:22 -0500

    Update script version and duration check condition

--- sabnzbd/video2.bash
+++ sabnzbd/video2.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-scriptVersion="4.4"
+scriptVersion="4.5"
 scriptName="Processor"
 dockerPath="/config/logs"
 
@@ -470,7 +470,7 @@ MAIN () {
   fi
 
   duration=$SECONDS
-  if [ $duration -gt 60 ]; then
+  if [ $duration -ge 60 ]; then
     echo "Completed in $(($duration / 60 )) minutes and $(($duration % 60 )) seconds!"
   else
     echo "Completed in $(($duration % 60 )) seconds!"
